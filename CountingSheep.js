4035504804f6ebaf267d5cc06ed67a4ada225969
coderrob22 <robertknott7@gmail.com>
2022-09-07 06:48:24 -0400

    Counting Sheep

---
+++ CountingSheep.js
@@ -0,0 +1,16 @@
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+  }
+
+  or 
+
+  function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    var num = 0;
+    
+    for(var i = 0; i < arrayOfSheep.length; i++)
+      if(arrayOfSheep[i] == true)
+        num++;
+        
+    return num;
+  }
