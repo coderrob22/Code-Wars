038121348de4d88c6c55b3bc93282f7a160c0226
coderrob22 <robertknott7@gmail.com>
2022-11-17 09:51:07 -0500

    if else and ternary

---
+++ JS 7 if else and ternary.js
@@ -0,0 +1,17 @@
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+// number	price (cents)
+// n < 5	100
+// n >= 5 and n < 10	95
+// n >= 10	90
+// You can use if..else or ternary operator to complete it.
+
+function saleHotdogs(n){
+    return n<5 ? n*100 : n<10 ? n*95 : n*90;
+  }
+
+//OR
+
+function saleHotdogs(n){
+    return n*(n<5?100:n<10?95:90);
+  }
