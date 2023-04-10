29b46e4be31840fc71b04638228dcb992f9e379c
coderrob22 <robertknott7@gmail.com>
2023-04-10 09:24:13 -0400

    dot calc

---
+++ dot calculator.js
@@ -0,0 +1,33 @@
+// You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, and dots again after the operator. The dots and the operator will be separated by one space.
+
+// Here are the following valid operators :
+
+// + Addition
+// - Subtraction
+// * Multiplication
+// // Integer Division
+// Your Work (Task)
+// You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+
+// Examples (Input => Output)
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
+
+function dotCalculator (equation) {
+	let [a,b,c] = equation.split(' '),
+      object= {
+        '+' : (x,y) => x+y,
+        '-' : (x,y) => x-y,
+        '*' : (x,y) => x*y,
+        '//' : (x,y) => x/y
+  
+      }
+  return '.'.repeat(object[b](a.length, c.length))
+}
